c5b8fa5ea21311eb567e9c8d05a38f30a9affbc7
EC2 Default User <ec2-user@ip-172-31-35-187.us-east-2.compute.internal>
2018-02-19 05:07:57 +0000

    rename Constants to constants

---
+++ src/app/constants.ts
@@ -0,0 +1,20 @@
+export class Constants {
+    public static  EVENTS = {
+        ENTRY_ADDED: 'entry-added',
+        SEARCH_FOOD_SELECTED: 'search-food-selected',
+        ADD_NEW_FOOD_SELECTED: 'add-new-selected',
+        FOOD_ADDED: 'food-added',
+        ADD_FOOD_CANCELED: 'add-food-canceled',
+        DATE_CHANGED: 'date-changed'
+    };
+
+    public static COMPONENTS = {
+        ADD_ENTRY_FORM: 'add-form',
+        DAY_ENTRIES: 'day-entries',
+        ADD_ENTRY: 'add-entry'
+    };
+
+    public static API = {
+        SERVER_BASE : 'http://localhost:8080/api'
+    }
+}
